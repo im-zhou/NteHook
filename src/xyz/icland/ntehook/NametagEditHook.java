c1b3acf7ff8ece34113546a6155009126c1dcafe
skyxingcheng233 <skyxingcheng233@outlook.com>
2022-02-24 14:50:40 +0800

    删除稚嫩的代码

--- src/xyz/icland/ntehook/NametagEditHook.java
+++ src/xyz/icland/ntehook/NametagEditHook.java
@@ -1,57 +1,35 @@
 package xyz.icland.ntehook;
 
 import com.nametagedit.plugin.NametagEdit;
-import com.nametagedit.plugin.api.data.Nametag;
-
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class NametagEditHook extends PlaceholderExpansion {
-	
-  public boolean canRegister() {
-    return Bukkit.getPluginManager().getPlugin(getPlugin()) != null;
-  }
-  
-  public String getAuthor() {
-    return "sky_xc";
-  }
-  
-  public String getIdentifier() {
-    return "nte";
-  }
-  
-  public String getPlugin() {
-    return "NametagEdit";
-  }
-  
-  public String getVersion() {
-    return "1.2";
-  }
-  
-  public String onPlaceholderRequest(Player p, String identifier) 
-  {
-    if (identifier.equals("prefix")) { 
-    	return getprefix(p);
-    	}
-    if (identifier.equals("suffix")) { 
-    	return getsuffix(p);
-    	}
-    
-    return "ERROR";
-  }
-  
-  private String getprefix(Player player) {
-	Nametag temp = NametagEdit.getApi().getNametag(player);
-    String name = temp.getPrefix();
-    return name;
-  }
-  
-  private String getsuffix(Player player) {
-	Nametag temp2 = NametagEdit.getApi().getNametag(player);
-    String name2 = temp2.getSuffix();
-    return name2;
-  }
-  
+
+    public boolean canRegister() {
+        return Bukkit.getPluginManager().getPlugin("Nametagedit") != null;
+    }
+
+    public String getAuthor() {
+        return "sky_xc";
+    }
+
+    public String getIdentifier() {
+        return "nte";
+    }
+
+    public String getVersion() {
+        return "1.3";
+    }
+
+    public String onPlaceholderRequest(Player p, String s) {
+        if (s.equals("prefix")) {
+            return NametagEdit.getApi().getNametag(p).getPrefix();
+        } else if (s.equals("suffix")) {
+            return NametagEdit.getApi().getNametag(p).getSuffix();
+        } else {
+            return null;
+        }
+    }
 }
